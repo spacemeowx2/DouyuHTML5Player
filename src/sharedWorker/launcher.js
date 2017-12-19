f9d9235e531945ed616b0b3ead1f2290f595a184
imspace <imspace@vip.qq.com>
2017-12-19 20:42:41 +0800

    delete sharedWorker demo code

--- src/sharedWorker/launcher.js
+++
@@ -1,7 +0,0 @@
-var worker = new SharedWorker(chrome.runtime.getURL('src/sharedWorker/sharedWorker.js'));
-worker.port.start();
-worker.port.onmessage = function(event) {
-    console.log('Received message', event.data);
-    window.lastMessage = event.data;
-};
-worker.port.postMessage('Hello');
